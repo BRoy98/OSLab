b7bded8f2278a1e752a48415d99ad0b5db749cd3
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2019-04-05 20:30:10 +0100

    print prime numbers through different function

--- multithreading.c
+++ multithreading.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 
+void printPrimeNumbers(int count)
+{
+    int printCount = 0;
+    int num = 1;
+    int i;
+    while (printCount < count)
+    {
+        num++;
+        for (i = 2; i <= num - 1; i)
+        {
+            if (num % i == 0)
+                break;
+            i++;
+        }
+        if (i >= num - 1)
+        {
+            printf("%d ", num);
+            count++;
+        }
+    }
+    printf("\n");
+}
+
 int main()
 {
     int numCount;
 
     printf("Enter the number of prime numbers you want to print: ");
     scanf("%d", &numCount);
+
+    if (numCount < 1)
+    {
+        printf("You must enter a value greater than 1");
+        return 1;
+    }
+    printf("First %d prime numbers are: ", numCount);
+    printPrimeNumbers(numCount);
 }
