5ca83245bc02218901a622c7379d1d3ec8d9afcf
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2019-04-06 10:35:10 +0100

    Add checking for invalid input
    Signed-off-by: Bishwajyoti Roy <bishwajyoti456@gmail.com>

--- bankers-algo.c
+++ bankers-algo.c
@@ -8,4 +8,16 @@ int main()
     scanf("%d", &processCount);
     printf("\nEnter the number resources: ");
     scanf("%d", &resourcesCount);
+
+    if (processCount > 6 || processCount < 1)
+    {
+        printf("\nProcess must be less than 6 and greter than 0!");
+    }
+    if (processCount > 4 || resourcesCount < 1)
+    {
+        printf("\nResources must be less than 4 and greter than 0!");
+    }
+
+    int allocArray[processCount][resourcesCount];
+    int maxArray[processCount][resourcesCount];
 }
