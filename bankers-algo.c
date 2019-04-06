7e78e4e324073367c784b6afc550dba6c66fdb36
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2019-04-06 11:38:18 +0100

    Get allocation and max values from user Add colors!

--- bankers-algo.c
+++ bankers-algo.c
@@ -2,22 +2,26 @@
 
 int processCount;
 int resourcesCount;
+char prsVals[4] = "ABCD";
+
+#define ANSI_BG_BLUE "\x1b[44;1m"
+#define ANSI_BG_RED "\x1b[41;1m"
+#define ANSI_BG_BLACK "\x1b[40;1m"
 
 int printTitle()
 {
     system("clear");
-    printf("\n\n..----------------------..\n");
-    printf("||  Banker's Algorithm  ||\n");
-    printf("||______________________||\n");
+    printf(ANSI_BG_BLUE "\n\n                            \n");
+    printf("     Banker's Algorithm     \n");
+    printf("                            \n" ANSI_BG_BLACK);
 }
 
 int takeInput()
 {
-
     printTitle();
     printf("\nEnter the number process: ");
     scanf("%d", &processCount);
-    printf("\nEnter the number resources: ");
+    printf("Enter the number resources: ");
     scanf("%d", &resourcesCount);
 
     if (processCount > 6 || processCount < 3)
@@ -40,11 +44,28 @@ int main()
     for (int a = 0; a < processCount; a++)
     {
         printTitle();
-        printf("\nEnter Process %d Values -> \n", a + 1);
+        printf(ANSI_BG_RED "                            \n");
+        printf("          Allocation        \n");
+        printf("                            \n" ANSI_BG_BLACK);
+        printf("\nEnter Process %d (P%d) Values  \n\n", a, a);
         for (int b = 0; b < resourcesCount; b++)
         {
-            // printf("\n");
-            // scanf("%d", &processCount);
+            printf("%c: ", prsVals[b]);
+            scanf("%d", &allocArray[a][b]);
+        }
+    }
+
+    for (int a = 0; a < processCount; a++)
+    {
+        printTitle();
+        printf(ANSI_BG_RED "                            \n");
+        printf("             MAX            \n");
+        printf("                            \n" ANSI_BG_BLACK);
+        printf("\nEnter Process %d (P%d) Values  \n\n", a, a);
+        for (int b = 0; b < resourcesCount; b++)
+        {
+            printf("%c: ", prsVals[b]);
+            scanf("%d", &allocArray[a][b]);
         }
     }
 }
